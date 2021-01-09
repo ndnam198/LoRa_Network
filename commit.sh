e5dea2e44c8c7356506d2eea4bebb0f61bd3e464
ndnam198 <ndnam198@gmail.com>
2021-01-09 14:54:22 +0700

    [01/09/21 14:54:22]: code tested

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ timestamp=$(date +"%D %T")
 message=$1
 git add -A
 git commit -m "[$timestamp]: $message"
-git push --force https://github.com/ndnam198/LoRaNetwork.git master
+git push --force https://github.com/ndnam198/LoRa_Network.git master
 echo "*******************************************************"
 echo "Time commit: [$timestamp]"
 echo "Commit message: \"$message\""
